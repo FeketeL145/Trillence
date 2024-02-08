531676fc48500259383e812faa2f2989fbc21bd7
B-Gabor20 <gaborbodnar92@gmail.com>
2024-02-08 18:50:28 +0100

    Register and Login Page small changes

--- FrontEnd/trillence/src/pages/Login.js
+++ FrontEnd/trillence/src/pages/Login.js
@@ -1,14 +1,13 @@
 function Login(){
     return(
-        <div className="container text-center text-justify bordertester">
+        <div className="container text-center text-justify">
         <div className="card text-center p-2 loginpagewidth">
             <form className="text-center">
-                <div className="form-group">
+                <div className="form-group m-2">
                     <label for="exampleInputEmail1">Email address</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email"/>
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
-                <div className="form-group">
+                <div className="form-group m-2">
                     <label for="exampleInputPassword1">Password</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                 </div>
--- FrontEnd/trillence/src/pages/Register.js
+++ FrontEnd/trillence/src/pages/Register.js
@@ -1,29 +1,32 @@
+
+
+
 function Register(){
     return(
         <div className="javisdmegpls container text-center d-flex align-items-center justify-content-center">
             <div className="text-center card p-5">
-            <form className="">
-                <div className="form-group">
+            <form className="form-group needs-validation">
+                <div className="form-group m-3">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email"/>
+                    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" required/>
                 </div>
-                <div className="form-group">
-                    <label>username</label>
-                    <input className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-                </div>
-                <div className="form-group">
-                    <label>Fullname</label>
-                    <input className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-                </div>
-                <div className="form-group">
+                <div className="form-group m-2">
                     <label>Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" required/>
                 </div>
-                <div className="form-group">
-                    <label>Password again</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password again"/>
+                <div className="form-group m-2">
+                    <label>Confirm Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Confirm Password" required/>
                 </div>
-                <button type="submit" className="btn btn-primary m-3">Login</button>
+                <div className="form-group m-2">
+                    <label>User Name</label>
+                    <input className="form-control" id="exampleInputPassword1" placeholder="User Name" required/>
+                </div>
+                <div className="form-group m-2">
+                    <label>Full Name</label>
+                    <input className="form-control" id="exampleInputPassword1" placeholder="Full Name" required/>
+                </div>
+                <button type="submit" className="btn btn-primary m-3">Register</button>
             </form>
             </div>
         </div>
